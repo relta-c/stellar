07dee4aed5038d14c4d2a4bb8d2cf6c0aa3472d0
delta047 <delta@chifumi.net>
2019-04-06 22:12:57 +0700

    Add Timer utility
    Signed-off-by: delta047 <delta@chifumi.net>

---
+++ src/main/java/net/chifumi/stellar/utils/Timer.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2019 Nattakit Hosapsin <delta@chifumi.net>
+ *
+ * This file is part of Stellar
+ * Stellar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  Stellar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Stellar.  If not, see <https://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
+
+package net.chifumi.stellar.utils;
+
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Represents timer, can be used to get delta time.
+ *
+ * @author Nattakit Hosapsin
+ * @version 1.0.2
+ * @since 1.0.2
+ */
+public class Timer {
+    private boolean started;
+    private float startTime;
+    private float lastRecordedTime;
+
+    /**
+     * Create a new {@link net.chifumi.stellar.utils.Timer}.
+     */
+    public Timer() {
+        started = false;
+        startTime = 0;
+        lastRecordedTime = 0;
+    }
+
+    @SuppressWarnings("NumericCastThatLosesPrecision")
+    public static float getCurrentTime() {
+        return (float) GLFW.glfwGetTime();
+    }
+
+    public void start() {
+        startTime = getCurrentTime();
+        lastRecordedTime = getCurrentTime();
+        started = true;
+    }
+
+    public float getDeltaTime() {
+        final float deltaTime;
+        if (started) {
+            deltaTime = getCurrentTime() - lastRecordedTime;
+            lastRecordedTime = getCurrentTime();
+        } else {
+            throw new IllegalStateException("Timer is not started");
+        }
+
+        return deltaTime;
+    }
+
+    public float getStartTime() {
+        return startTime;
+    }
+
+    public float getLastRecordedTime() {
+        return lastRecordedTime;
+    }
+
+    public float getSumRecordedTime() {
+        return getCurrentTime() - startTime;
+    }
+
+    public void reset() {
+        lastRecordedTime = getCurrentTime();
+    }
+
+    public void stop() {
+        startTime = 0;
+        lastRecordedTime = 0;
+        started = false;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+}
